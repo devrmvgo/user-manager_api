1a5f2ad36b73b90c5f5d09d239d4bc80f03113fb
Luiz Felipe Alves <felipealvesalp@gmail.com>
2021-07-29 21:26:01 -0300

    routes and controller methods

--- start/routes.js
+++ start/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 const UserController = require('../app/controllers/UserController');
 
 routes.get('/', UserController.index)
+routes.post('/', UserController.index)
+routes.put('/', UserController.index)
+routes.delete('/', UserController.index)
 
 module.exports = routes;
